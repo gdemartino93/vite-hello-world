5a35d27411b9c1f4ead0ce3e9464947d606eeb46
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2022-12-07 14:20:04 +0100

    creato componene AppTitle

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+export default{
+    data() {
+        return {
+            msg : "La mia prima app con vite"
+        }
+    }
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  
+</template>
+
+<style scoped>
+h1{
+  color: red;
+}
+
+</style>
